2b359f4ad219b7475d323165d6dbdef876f82687
Daniel Barros <dani.barros@me.com>
2023-07-28 15:50:40 -0700

    First version of the adapter, pegged to 2.8.0 for now (#1)
    * First version of the adapter, pegged to 2.8.0 for now
    
    * Update min Swift version and deployment target to match Usercentrics SDK 2.8.0
    
    * Fix double spaces

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# chartboost-core-ios-cmp-usercentrics
-Chartboost Core Usercentrics CMP Module
+# Chartboost Core Usercentrics Adapter
+
+The Chartboost Core Usercentrics adapter mediates Usercentrics via the Chartboost Core SDK.
+
+## Minimum Requirements
+
+| Plugin | Version |
+| ------ | ------ |
+| Chartboost Core SDK | 0.0.0+ |
+| Cocoapods | 1.11.3+ |
+| iOS | 11.0+ |
+| Xcode | 14.1+ |
+
+## Integration
+
+In your `Podfile`, add the following entry:
+```
+pod 'ChartboostCoreAdapterUsercentrics'
+```
+
+## Contributions
+
+We are committed to a fully transparent development process and highly appreciate any contributions. Our team regularly monitors and investigates all submissions for the inclusion in our official adapter releases.
+
+Refer to our [CONTRIBUTING](CONTRIBUTING.md) file for more information on how to contribute.
+
+## License
+
+Refer to our [LICENSE](LICENSE.md) file for more information.
---
+++ Source/UsercentricsAdapter.swift
@@ -0,0 +1,309 @@
+//
+//  UsercentricsAdapter.swift
+//  CoreCanary
+//
+//  Created by Daniel Barros on 7/27/23.
+//
+
+import ChartboostCoreSDK
+import Usercentrics
+import UsercentricsUI
+
+// WARNING for adapter developers:
+// All accesses to the Usercentrics SDK APIs must be wrapped by a call to `UsercentricsCore.isReady()`.
+// Usercentrics SDK will crash if its APIs are used but the SDK is not ready.
+
+/// Chartboost Core Consent Usercentrics adapter.
+public final class UsercentricsAdapter: ConsentAdapter {
+
+    public enum InitializationError: String, Error {
+        case usercentricsOptionsNotAvailable
+    }
+
+    public let moduleID = "usercentrics"
+
+    public let moduleVersion = "0.1.0"
+
+    public weak var observer: ConsentObserver?
+
+    /// The name of the Usercentrics Data Processing Service (DPS) defined in the Usercentrics dashboard for the Chartboost Core SDK.
+    let chartboostCoreDPSName: String
+
+    /// The Usercentrics options used to configure the Usercentrics SDK.
+    let options: UsercentricsOptions?
+
+    /// The default value for `chartboostCoreDPSName` when none is provided.
+    public static var defaultChartboostCoreDPSName = "ChartboostCore"
+
+    /// The settings provided when creating the Usercentrics banner.
+    /// This property may be modified before the first call to ``showConsentDialog()`` to customize the banner created by the adapter.
+    /// Changes afterwards have no effect.
+    public static var bannerSettings: BannerSettings?
+
+    /// The latest shouldCollectConsent fetched value.
+    private var cachedShouldCollectConsent: Bool?
+
+    /// The latest consentStatus fetched value.
+    private var cachedConsentStatus: ConsentStatus?
+
+    /// The latest TCF string fetched value.
+    private var cachedTCFString: String?
+
+    /// The latest USP string fetched value.
+    private var cachedUSPString: String?
+
+    /// The latest CCPA string fetched value.
+    private var cachedCCPAString: ConsentValue?
+
+    /// The Usercentrics banner used to display consent dialogs.
+    /// It may be customized by the user by modifying the static property ``UsercentricsAdapter.bannerSettings``.
+    private lazy var banner = UsercentricsBanner(bannerSettings: Self.bannerSettings)
+
+    /// Instantiates a UsercentricsAdapter module which can be passed on a call to ``ChartboostCore.initializeSDK()`
+    /// - parameter options: The options to initialize Usercentrics with. Refer to the Usercentrics documentation:
+    /// https://docs.usercentrics.com/cmp_in_app_sdk/latest/getting_started/configure/
+    /// - parameter chartboostCoreDPSName: The name for the Chartboost Core DPS that matches the one set on the Usercentrics dashboard.
+    public init(options: UsercentricsOptions, chartboostCoreDPSName: String = UsercentricsAdapter.defaultChartboostCoreDPSName) {
+        self.options = options
+        self.chartboostCoreDPSName = chartboostCoreDPSName
+    }
+
+    public init(credentials: [String : Any]?) {
+        self.options = Self.usercentricsOptions(from: credentials?["options"] as? [String: Any])
+        self.chartboostCoreDPSName = credentials?["coreDPSName"] as? String ?? Self.defaultChartboostCoreDPSName
+    }
+
+    public func initialize(completion: @escaping (Error?) -> Void) {
+        // Fail if no options provided on init
+        guard let options else {
+            print("[Usercentrics Adapter] Failed to initialize Usercentrics: no options available.")
+            completion(InitializationError.usercentricsOptionsNotAvailable)
+            return
+        }
+        // Configure the SDK
+        print("[Usercentrics Adapter] Configuring SDK")
+        UsercentricsCore.configure(options: options)
+
+        // Start observing consent changes and reporting updates to the observer object
+        startObservingConsentChanges()
+
+        // Fetch the initial consent status
+        fetchConsentInfo()
+
+        // Report success immediately. If configuration fails it will be retried when calling any Usercentrics method.
+        completion(nil)
+    }
+
+    public var shouldCollectConsent: Bool {
+        cachedShouldCollectConsent ?? true
+    }
+
+    public var consentStatus: ConsentStatus {
+        cachedConsentStatus ?? .unknown
+    }
+
+    public var consents: [ConsentStandard : ConsentValue] {
+        var consents: [ConsentStandard: ConsentValue] = [:]
+        consents[.tcf] = cachedTCFString.map(ConsentValue.init(stringLiteral:))
+        consents[.usp] = cachedUSPString.map(ConsentValue.init(stringLiteral:))
+        consents[.ccpa] = cachedCCPAString
+        return consents
+    }
+
+    public func setConsentStatus(_ status: ConsentStatus, completion: @escaping (Bool) -> Void) {
+        print("[Usercentrics Adapter] Setting consent status...")
+        UsercentricsCore.isReady(onSuccess: { [weak self] _ in
+            // SDK ready
+            switch status {
+            case .granted:
+                // Accept all consents
+                print("[Usercentrics Adapter] Accept all explicit consents")
+                UsercentricsCore.shared.acceptAll(consentType: .explicit_)
+                // Fetch consent info. Usercentrics does not report updates triggered by programmatic changes.
+                self?.fetchConsentInfo()
+
+            case .denied:
+                // Accept all consents
+                print("[Usercentrics Adapter] Deny all explicit consents")
+                UsercentricsCore.shared.denyAll(consentType: .explicit_)
+                // Fetch consent info. Usercentrics does not report updates triggered by programmatic changes.
+                self?.fetchConsentInfo()
+
+            case .unknown:
+                // Reset all consents
+                print("[Usercentrics Adapter] Reset")
+                UsercentricsCore.reset()
+
+                // Clear cached consent info. Usercentrics does not report updates triggered by programmatic changes.
+                self?.resetCachedConsentInfo()
+
+                // Usercentrics needs to be configured again after a call to reset()
+                self?.initialize(completion: { _ in })
+            }
+            completion(true)
+        }, onFailure: { error in
+            // SDK not ready
+            print("[Usercentrics Adapter] SDK not ready: \(error)")
+            completion(false)
+        })
+    }
+
+    public func showConsentDialog(_ type: ConsentDialogType, from viewController: UIViewController, completion: @escaping (Bool) -> Void) {
+        print("[Usercentrics Adapter] Showing consent dialog...")
+        UsercentricsCore.isReady(onSuccess: { [weak self] _ in
+            // SDK ready
+            switch type {
+            case .concise:
+                self?.banner.showFirstLayer(hostView: viewController) { userResponse in
+                    print("[Usercentrics Adapter] 1st layer response: \(userResponse)")
+                }
+                completion(true)
+            case .detailed:
+                self?.banner.showSecondLayer(hostView: viewController) { userResponse in
+                    print("[Usercentrics Adapter] 2nd layer response: \(userResponse)")
+                }
+                completion(true)
+            default:
+                print("[Usercentrics Adapter] Unknown consent dialog type: \(type)")
+                completion(false)
+            }
+        }, onFailure: { error in
+            // SDK not ready
+            print("[Usercentrics Adapter] SDK not ready: \(error)")
+            completion(false)
+        })
+    }
+
+    /// Creates a UsercentricsOptions object with the dashboard information provided in a JSON dictionary.
+    private static func usercentricsOptions(from dictionary: [String: Any]?) -> UsercentricsOptions? {
+        guard let dictionary else {
+            return nil
+        }
+        let loggerLevel: UsercentricsLoggerLevel
+        switch dictionary["loggerLevel"] as? String {
+        case "none":
+            loggerLevel = .none
+        case "error":
+            loggerLevel = .error
+        case "warning":
+            loggerLevel = .warning
+        case "debug":
+            loggerLevel = .debug
+        default:
+            loggerLevel = .debug
+        }
+        return UsercentricsOptions(
+            settingsId: dictionary["settingsId"] as? String ?? "",
+            defaultLanguage: dictionary["defaultLanguage"] as? String ?? "en",
+            version: dictionary["version"] as? String ?? "latest",
+            timeoutMillis: dictionary["timeoutMillis"] as? Int64 ?? 5_000,
+            loggerLevel: loggerLevel,
+            ruleSetId: dictionary["ruleSetId"] as? String ?? "",
+            consentMediation: dictionary["consentMediation"] as? Bool ?? false
+        )
+    }
+
+    /// Makes the adapter begin to receive consent updates from the Usercentrics SDK.
+    private func startObservingConsentChanges() {
+        print("[Usercentrics Adapter] Starting to observe consent changes")
+
+        UsercentricsEvent.shared.onConsentUpdated { [weak self] payload in
+            print("[Usercentrics Adapter] onConsentUpdated with payload:\n\(payload)")
+            guard let self else { return }
+
+            // We discard the payload and just pull all the info directly from the SDK.
+            // The result should be the same, we do this to simplify our implementation and prevent
+            // data from getting out of sync.
+            self.fetchConsentInfo()
+        }
+    }
+
+    /// Pulls all the consent info from the Usercentrics SDK, saves it in the adapter's internal cache,
+    /// and reports updates to the adapter observer.
+    private func fetchConsentInfo() {
+        UsercentricsCore.isReady(onSuccess: { [weak self] status in
+            guard let self else { return }
+
+            // Should Collect Consent
+            self.cachedShouldCollectConsent = status.shouldCollectConsent
+
+            // Consent Status
+            let newConsentStatus: ConsentStatus?
+            if let coreDPS = status.consents.first(where: { $0.dataProcessor == self.chartboostCoreDPSName }) {
+                newConsentStatus = coreDPS.status ? .granted : .denied
+            } else {
+                print("[Usercentrics Adapter] ChartboostCore DPS not found in payload, expected one named '\(self.chartboostCoreDPSName)'")
+                newConsentStatus = nil
+            }
+            if self.cachedConsentStatus != newConsentStatus {
+                self.cachedConsentStatus = newConsentStatus
+                self.observer?.onConsentStatusChange(consentStatus)
+            }
+
+            // TCF string
+            UsercentricsCore.shared.getTCFData { [weak self] tcfData in
+                guard let self else { return }
+                let newTCFString = tcfData.tcString.isEmpty ? nil : tcfData.tcString
+                if self.cachedTCFString != newTCFString {
+                    self.cachedTCFString = newTCFString
+                    self.observer?.onConsentChange(standard: .tcf, value: newTCFString.map(ConsentValue.init(stringLiteral:)))
+                }
+            }
+
+            // USP String
+            let uspData = UsercentricsCore.shared.getUSPData()
+            let newUSPString = uspData.uspString.isEmpty ? nil : uspData.uspString
+            if self.cachedUSPString != newUSPString {
+                self.cachedUSPString = newUSPString
+                self.observer?.onConsentChange(standard: .usp, value: newUSPString.map(ConsentValue.init(stringLiteral:)))
+            }
+
+            // CCPA String
+            let newCCPAString: ConsentValue?
+            if let ccpaOptedOut = uspData.optedOut {
+                newCCPAString = ccpaOptedOut.boolValue ? .denied : .granted
+            } else {
+                newCCPAString = nil
+            }
+            if self.cachedCCPAString != newCCPAString {
+                self.cachedCCPAString = newCCPAString
+                self.observer?.onConsentChange(standard: .ccpa, value: newCCPAString)
+            }
+
+        }, onFailure: { [weak self] error in
+            guard let self else { return }
+            print("[Usercentrics Adapter] SDK not ready: \(error)")
+            self.resetCachedConsentInfo()
+        })
+    }
+
+    /// Clears the adapter internal cache and reports updates to the observer.
+    private func resetCachedConsentInfo() {
+        // Should Collect Consent
+        self.cachedShouldCollectConsent = nil
+
+        // Consent Status
+        if self.cachedConsentStatus != nil {
+            self.cachedConsentStatus = nil
+            self.observer?.onConsentStatusChange(consentStatus)
+        }
+
+        // TCF string
+        if self.cachedTCFString != nil {
+            self.cachedTCFString = nil
+            self.observer?.onConsentChange(standard: .tcf, value: nil)
+        }
+
+        // USP String
+        if self.cachedUSPString != nil {
+            self.cachedUSPString = nil
+            self.observer?.onConsentChange(standard: .usp, value: nil)
+        }
+
+        // CCPA String
+        if self.cachedCCPAString != nil {
+            self.cachedCCPAString = nil
+            self.observer?.onConsentChange(standard: .ccpa, value: nil)
+        }
+    }
+}
